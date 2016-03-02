53786c68e91b1db35cc0a91e4cf6a79c365a8f1d
pablok47 <pkaparis@gmail.com>
2016-03-02 17:23:08 -0500

    pulled out the hours, minutes and seconds from the Date object located in the displayTime function

--- clock.js
+++ clock.js
@@ -2,6 +2,11 @@ $document.ready(function(){
 
   function displayTime() {
 
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
   }
 
 });
