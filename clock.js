11c3cb0a52b3bbbf01aae774323b51daa3a64b2e
pablok47 <pkaparis@gmail.com>
2016-03-02 19:07:48 -0500

    added the 0 in front of single digits using if-statements; added the AM and PM feature; also added a bit of styling

--- clock.js
+++ clock.js
@@ -9,8 +9,26 @@ function displayTime() {
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
 
+    var meridiem = "AM"; // this is the default
+    if (hours >= 12) {
+      meridiem = "PM"
+    }
+
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+      // this adds a 0 in front of single digits so 9 shows up as 09
+      seconds = "0" + seconds;
+    }
+
     // this gets the clock running in the html
-    document.body.innerHTML = hours + ":" + minutes + ":" + seconds;
+    document.body.innerHTML = hours + ":" + minutes + ":" + seconds + " " + meridiem;
 
   }
 
